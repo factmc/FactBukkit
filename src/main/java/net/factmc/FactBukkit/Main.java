58c5b20505b77a6181d47042431ebd42f35dc243
TrollyLoki <trollyloki@factmc.net>
2020-07-18 16:42:01 -0400

    Set collision rule of rank teams to always

--- src/main/java/net/factmc/FactBukkit/Main.java
+++ src/main/java/net/factmc/FactBukkit/Main.java
@@ -35,214 +35,211 @@ import net.luckperms.api.model.group.Group;
 import net.milkbowl.vault.economy.Economy;
 
 public class Main extends JavaPlugin implements Listener {
-	
+
 	public static JavaPlugin plugin;
-	
-	//public static List<Object> roles = new ArrayList<Object>();
-	
+
+	// public static List<Object> roles = new ArrayList<Object>();
+
 	public static Economy econ = null;
 	public static boolean useVanish = true;
 	public static boolean gpInstalled = false;
 	public static boolean disableRankTag = false;
 	private static Scoreboard sb;
-	
-    @Override
-    public void onEnable() {
-    	plugin = this;
-    	saveDefaultConfig();
-    	
-    	/*useFactions = getServer().getPluginManager().getPlugin("Factions") != null;
-    	if (useFactions) {
-    		plugin.getLogger().info("Found Factions. Using Factions for prefixes");
-    		
-    		roles.clear();
-            roles.add(Rel.RECRUIT);
-            roles.add(Rel.MEMBER);
-            roles.add(Rel.OFFICER);
-            roles.add(Rel.LEADER);
-    	}*/
-    	
-    	useVanish = getServer().getPluginManager().getPlugin("SuperVanish") != null;
-    	if (useVanish) {
-    		plugin.getLogger().info("Found SuperVanish. Monitoring its events");
-    	}
-    	
-    	gpInstalled = getServer().getPluginManager().getPlugin("GriefPrevention") != null;
-    	if (gpInstalled) {
-    		plugin.getLogger().info("Blocking claiming shovel mining");
-    	}
-    	
-    	getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
-    	setupEconomy();
-    	
-    	/*
-    	disableRankTag = getServer().getPluginManager().getPlugin("FactHub") != null;
-    	if (disableRankTag) {
-    		plugin.getLogger().info("Found FactHub. Disabling Listeners");
-    	}
-    	*/
-    	
-    	registerEvents();
-    	registerCommands();
-    	
-    	loadBossBar();
-    	plugin.getLogger().info("Loaded BossBar");
-    	
-    	/*RegisteredServiceProvider<Permission> permRSP = getServer().getServicesManager().getRegistration(Permission.class);
-        perms = permRSP.getProvider();
-        plugin.getLogger().info("Connected to " + perms.getName());*/
-    	
-        if (!disableRankTag) {
-        	sb = Bukkit.getScoreboardManager().getMainScoreboard();
-	    	registerTeams(sb);
-	        plugin.getLogger().info("Registered Teams");
-        }
-        
-        if (getConfig().getBoolean("clearlag.enabled")) {
-        	int interval = getConfig().getInt("clearlag.interval");
-        	Integer[] warnings = getConfig().getIntegerList("clearlag.warnings").toArray(new Integer[0]);
-        	
-        	String warningMessage = ChatColor.translateAlternateColorCodes('&', getConfig().getString("clearlag.warning-message"));
-        	String clearMessage = ChatColor.translateAlternateColorCodes('&', getConfig().getString("clearlag.clear-message"));
-        	boolean hideMessages = getConfig().getBoolean("clearlag.hide-messages");
-        	
-        	Class<?>[] removeTypes = ClearLagTask.getTypeClasses();
-        	new ClearLagTask(this, interval, warnings, warningMessage, clearMessage, hideMessages, removeTypes);
-        }
-    }
-    
-    @Override
-    public void onDisable() {
-    	Bukkit.getScheduler().cancelTasks(plugin);
-    	plugin.getLogger().info("Cancelled Tasks");
-    	
-    	plugin = null;
-    }
-    
-    private boolean setupEconomy() {
-        if (getServer().getPluginManager().getPlugin("Vault") == null) {
-            return false;
-        }
-        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
-        if (rsp == null) {
-            return false;
-        }
-        econ = rsp.getProvider();
-        return econ != null;
-    }
-    
-    public void registerEvents() {
-    	
-    	getServer().getPluginManager().registerEvents(new StatsGUI(), plugin);
-    	getServer().getPluginManager().registerEvents(new ServerGUI(), plugin);
-    	
-    	if (useVanish) {
-    		getServer().getPluginManager()
-    				.registerEvents(new VanishEvents(), plugin);
-    	}
-    	
-    	if (gpInstalled) {
-    		getServer().getPluginManager()
-    				.registerEvents(new ClaimingShovelBlocker(), plugin);
-    	}
-    	
-    	if (!disableRankTag) {
-	    	getServer().getPluginManager()
-	    			.registerEvents(new JoinEvents(), plugin);
-	    	new LuckPermsEvents();
-    	}
-    	
-    }
-    
-    public void registerCommands() {
-    	//plugin.getCommand("cmd").setExecutor(this);
-    	getCommand("stats").setExecutor(new StatsCommand());
-    	getCommand("points").setExecutor(new PointsCommand());
-    	getCommand("rtag-update").setExecutor(new ReloadCommand());
-    	getCommand("vote").setExecutor(new VoteCommand());
-    	getCommand("servers").setExecutor(new ServersCommand());
-    	getCommand("clearlag").setExecutor(new ClearlagCommand());
-    	
-    	SignEditCommand.load();
-    }
-    
-    public void loadBossBar() {
-    	List<String> titles = new ArrayList<String>();
-    	for (String title : getConfig().getStringList("bossbar.title")) {
-    		titles.add(ChatColor.translateAlternateColorCodes('&', title));
-    	}
-    	
-    	List<BarColor> colors = new ArrayList<BarColor>();
-    	for (String color : getConfig().getStringList("bossbar.color")) {
-    		colors.add(BarColor.valueOf(color));
-    	}
-    	List<BarStyle> styles = new ArrayList<BarStyle>();
-    	for (String style : getConfig().getStringList("bossbar.style")) {
-    		styles.add(BarStyle.valueOf(style));
-    	}
-    	
-    	int rate = (int) (getConfig().getDouble("bossbar.rate") * 20);
-    	new CustomBossbar(plugin, titles, colors, styles, rate);
-    }
-    
-    public static JavaPlugin getPlugin() {
-        return plugin;
-    }
-    
-    public static Scoreboard getScoreboard() {
-    	return sb;
-    }
-    
-    
-    public static void registerTeams(Scoreboard sb) {
-    	
-    	/*if (useFactions) {
-    		for (Team team : sb.getTeams()) {
-        		if (team.getName().startsWith("f")) {
-        			team.unregister();
-        		}
-        	}
-    		
-    		for (Faction faction : FactionColl.get().getAll()) {
-    			TeamManager.updateTeam(faction);
-    		}
-    	}*/
-    	
+
+	@Override
+	public void onEnable() {
+		plugin = this;
+		saveDefaultConfig();
+
+		/*
+		 * useFactions = getServer().getPluginManager().getPlugin("Factions") != null;
+		 * if (useFactions) {
+		 * plugin.getLogger().info("Found Factions. Using Factions for prefixes");
+		 * 
+		 * roles.clear(); roles.add(Rel.RECRUIT); roles.add(Rel.MEMBER);
+		 * roles.add(Rel.OFFICER); roles.add(Rel.LEADER); }
+		 */
+
+		useVanish = getServer().getPluginManager().getPlugin("SuperVanish") != null;
+		if (useVanish) {
+			plugin.getLogger().info("Found SuperVanish. Monitoring its events");
+		}
+
+		gpInstalled = getServer().getPluginManager().getPlugin("GriefPrevention") != null;
+		if (gpInstalled) {
+			plugin.getLogger().info("Blocking claiming shovel mining");
+		}
+
+		getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
+		setupEconomy();
+
+		/*
+		 * disableRankTag = getServer().getPluginManager().getPlugin("FactHub") != null;
+		 * if (disableRankTag) {
+		 * plugin.getLogger().info("Found FactHub. Disabling Listeners"); }
+		 */
+
+		registerEvents();
+		registerCommands();
+
+		loadBossBar();
+		plugin.getLogger().info("Loaded BossBar");
+
+		/*
+		 * RegisteredServiceProvider<Permission> permRSP =
+		 * getServer().getServicesManager().getRegistration(Permission.class); perms =
+		 * permRSP.getProvider(); plugin.getLogger().info("Connected to " +
+		 * perms.getName());
+		 */
+
+		if (!disableRankTag) {
+			sb = Bukkit.getScoreboardManager().getMainScoreboard();
+			registerTeams(sb);
+			plugin.getLogger().info("Registered Teams");
+		}
+
+		if (getConfig().getBoolean("clearlag.enabled")) {
+			int interval = getConfig().getInt("clearlag.interval");
+			Integer[] warnings = getConfig().getIntegerList("clearlag.warnings").toArray(new Integer[0]);
+
+			String warningMessage = ChatColor.translateAlternateColorCodes('&',
+					getConfig().getString("clearlag.warning-message"));
+			String clearMessage = ChatColor.translateAlternateColorCodes('&',
+					getConfig().getString("clearlag.clear-message"));
+			boolean hideMessages = getConfig().getBoolean("clearlag.hide-messages");
+
+			Class<?>[] removeTypes = ClearLagTask.getTypeClasses();
+			new ClearLagTask(this, interval, warnings, warningMessage, clearMessage, hideMessages, removeTypes);
+		}
+	}
+
+	@Override
+	public void onDisable() {
+		Bukkit.getScheduler().cancelTasks(plugin);
+		plugin.getLogger().info("Cancelled Tasks");
+
+		plugin = null;
+	}
+
+	private boolean setupEconomy() {
+		if (getServer().getPluginManager().getPlugin("Vault") == null) {
+			return false;
+		}
+		RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+		if (rsp == null) {
+			return false;
+		}
+		econ = rsp.getProvider();
+		return econ != null;
+	}
+
+	public void registerEvents() {
+
+		getServer().getPluginManager().registerEvents(new StatsGUI(), plugin);
+		getServer().getPluginManager().registerEvents(new ServerGUI(), plugin);
+
+		if (useVanish) {
+			getServer().getPluginManager().registerEvents(new VanishEvents(), plugin);
+		}
+
+		if (gpInstalled) {
+			getServer().getPluginManager().registerEvents(new ClaimingShovelBlocker(), plugin);
+		}
+
+		if (!disableRankTag) {
+			getServer().getPluginManager().registerEvents(new JoinEvents(), plugin);
+			new LuckPermsEvents();
+		}
+
+	}
+
+	public void registerCommands() {
+		// plugin.getCommand("cmd").setExecutor(this);
+		getCommand("stats").setExecutor(new StatsCommand());
+		getCommand("points").setExecutor(new PointsCommand());
+		getCommand("rtag-update").setExecutor(new ReloadCommand());
+		getCommand("vote").setExecutor(new VoteCommand());
+		getCommand("servers").setExecutor(new ServersCommand());
+		getCommand("clearlag").setExecutor(new ClearlagCommand());
+
+		SignEditCommand.load();
+	}
+
+	public void loadBossBar() {
+		List<String> titles = new ArrayList<String>();
+		for (String title : getConfig().getStringList("bossbar.title")) {
+			titles.add(ChatColor.translateAlternateColorCodes('&', title));
+		}
+
+		List<BarColor> colors = new ArrayList<BarColor>();
+		for (String color : getConfig().getStringList("bossbar.color")) {
+			colors.add(BarColor.valueOf(color));
+		}
+		List<BarStyle> styles = new ArrayList<BarStyle>();
+		for (String style : getConfig().getStringList("bossbar.style")) {
+			styles.add(BarStyle.valueOf(style));
+		}
+
+		int rate = (int) (getConfig().getDouble("bossbar.rate") * 20);
+		new CustomBossbar(plugin, titles, colors, styles, rate);
+	}
+
+	public static JavaPlugin getPlugin() {
+		return plugin;
+	}
+
+	public static Scoreboard getScoreboard() {
+		return sb;
+	}
+
+	public static void registerTeams(Scoreboard sb) {
+
+		/*
+		 * if (useFactions) { for (Team team : sb.getTeams()) { if
+		 * (team.getName().startsWith("f")) { team.unregister(); } }
+		 * 
+		 * for (Faction faction : FactionColl.get().getAll()) {
+		 * TeamManager.updateTeam(faction); } }
+		 */
+
 		for (Group raw : LuckPermsProvider.get().getGroupManager().getLoadedGroups()) {
 			String group = raw.getName();
-    		for (int i = 0; i < 2; i++) {
-    			String add = "";
-    			if (i > 0) {
-    				if (!useVanish) break;
-    				add = "v";
-    			}
-    			
-        		Team team; String teamName = "rank_" + group + add;
-	        	try {
-	        		team = sb.registerNewTeam(teamName);
-	        	} catch (IllegalArgumentException e) {
-	        		team = sb.getTeam(teamName);
-	        	}
-	        	
-	        	if (!group.equalsIgnoreCase("default")) {
-		        	String prefix = CoreUtils.getPrefix(group);
-		        	team.setPrefix(prefix);
-	        	}
-	        	if (i > 0) {
-	        		team.setSuffix(ChatColor.GRAY + "[HIDDEN]");
-	        		team.setOption(Option.COLLISION_RULE, OptionStatus.NEVER);
-	        	}
-	        	
-    		}
-        }
-		
-		/*if (useVanish) {
-			Team oldTeam = sb.getTeam("rank_vanished");
-    		if (oldTeam != null) oldTeam.unregister();
-        	Team team = sb.registerNewTeam("rank_vanished");
-    		team.setSuffix(ChatColor.GRAY + "[HIDDEN]");
-		}*/
-    	
-    }
-    
+			for (int i = 0; i < 2; i++) {
+				String add = "";
+				if (i > 0) {
+					if (!useVanish)
+						break;
+					add = "v";
+				}
+
+				Team team;
+				String teamName = "rank_" + group + add;
+				try {
+					team = sb.registerNewTeam(teamName);
+				} catch (IllegalArgumentException e) {
+					team = sb.getTeam(teamName);
+				}
+
+				team.setOption(Option.COLLISION_RULE, OptionStatus.ALWAYS);
+
+				if (!group.equalsIgnoreCase("default")) {
+					String prefix = CoreUtils.getPrefix(group);
+					team.setPrefix(prefix);
+				}
+				if (i > 0) {
+					team.setSuffix(ChatColor.GRAY + "[HIDDEN]");
+					team.setOption(Option.COLLISION_RULE, OptionStatus.NEVER);
+				}
+
+			}
+		}
+
+		/*
+		 * if (useVanish) { Team oldTeam = sb.getTeam("rank_vanished"); if (oldTeam !=
+		 * null) oldTeam.unregister(); Team team = sb.registerNewTeam("rank_vanished");
+		 * team.setSuffix(ChatColor.GRAY + "[HIDDEN]"); }
+		 */
+
+	}
+
 }
